d771d95c03209e6073d83fee1b44d6aa849ba7af
Tomasz <tomasso.holy@gmail.com>
2018-08-01 21:23:18 +0200

    Made caret blinking css side, changed span to input, added missing dependency

--- src/ui/components/inputLine/inputLine.tsx
+++ src/ui/components/inputLine/inputLine.tsx
@@ -1,79 +1,98 @@
 import { h, Component } from 'preact';
-import { bind } from 'decko';
+import {bind} from 'decko';
+
+const TERMINAL_LINE_PREFIX: string = '>';
+const CARET_BLINKING_CLASS: string = 'blinking';
+const CARET_BLINK_PAUSE_TIME_MS: number = 300;
 
 interface InputLineState {
     terminalText: string;
+    caretBlinking: boolean;
 }
 
 export class InputLine extends Component<{}, InputLineState> {
-    private terminalInput: HTMLElement;
-    private commandPrompt: HTMLElement;
+    private terminalInput: HTMLInputElement;
+    private caret: HTMLElement;
+    private caretTimer: any;
 
     constructor() {
         super();
-        this.state = { terminalText: '' };
+        this.state = { terminalText: '', caretBlinking: true};
     }
 
     public render(): JSX.Element {
         return (
             <div className='input-line'>
-                <span>DevosCorp:~$ </span>
+                <span>{TERMINAL_LINE_PREFIX}</span>
                 <span className='terminal-text-inputed'>{this.state.terminalText}</span>
                 <span
-                    className='command-prompt'
-                    ref={(element: HTMLElement) => this.commandPrompt = element}>_</span>
-                <span
+                    className={`command-prompt ${this.state.caretBlinking ? CARET_BLINKING_CLASS : ''}`}
+                    ref={(element: HTMLElement) => this.caret = element}>_</span>
+                <input
                     className='terminal-input'
-                    contentEditable={true}
-                    ref={(element: HTMLElement) => this.terminalInput = element}>
-                    {this.state.terminalText}
-                </span>
+                    type='text'
+                    value={this.state.terminalText}
+                    ref={(element: HTMLInputElement) => this.terminalInput = element}/>
             </div>
         );
     }
 
     public componentDidMount(): void {
         this.terminalInput.addEventListener('input', this.onTerminalInputChange);
-        this.terminalInput.addEventListener('keydown', this.watchEnterKey);
+        this.terminalInput.addEventListener('keydown', this.watchKeys);
+        this.terminalInput.addEventListener('keyup', this.watchCaretPosition);
         document.addEventListener('click', this.clickOutsideInputListener);
-        this.terminalInput.focus();
-        this.blinkCommandPrompt();
+        this.focusToTerminal();
     }
 
     public componentWillUnmount(): void {
         this.terminalInput.removeEventListener('input', this.onTerminalInputChange);
-        this.terminalInput.removeEventListener('keydown', this.watchEnterKey);
+        this.terminalInput.removeEventListener('keydown', this.watchKeys);
+        this.terminalInput.removeEventListener('keyup', this.watchCaretPosition);
         document.removeEventListener('click', this.clickOutsideInputListener);
     }
 
     @bind()
     private onTerminalInputChange(): void {
-        console.log(`terminal input changed`, this.terminalInput.textContent);
-        const terminalText: string = this.terminalInput.textContent;
+        console.log(`terminal input changed`, this.terminalInput.value);
+        const terminalText: string = this.terminalInput.value;
         const state: InputLineState = Object.assign(this.state, {terminalText});
         this.setState(state);
+        this.pauseCaretBlinking();
     }
 
     @bind()
-    private watchEnterKey(event: KeyboardEvent): void {
+    private watchKeys(event: KeyboardEvent): void {
         if (event.key === 'Enter') {
            event.preventDefault();
+        } else {
+            // console.log(this.terminalInput.selectionS)
         }
     }
 
     @bind()
-    private clickOutsideInputListener(): void {
-        this.terminalInput.focus();
+    private watchCaretPosition(): void {
+        console.log(this.terminalInput.selectionStart);
     }
 
     @bind()
-    private blinkCommandPrompt(): void {
-        setInterval(() => {
-            if (this.commandPrompt.classList.contains('hidden')) {
-                this.commandPrompt.classList.remove('hidden');
-            } else {
-                this.commandPrompt.classList.add('hidden');
-            }
-        }, 1000);
+    private clickOutsideInputListener(): void {
+        this.focusToTerminal();
+    }
+
+    private focusToTerminal(): void {
+        this.terminalInput.focus();
+    }
+
+    private pauseCaretBlinking(): void {
+        window.clearTimeout(this.caretTimer);
+        this.setCaretBlink(false);
+        this.caretTimer = setTimeout(() => {
+            this.setCaretBlink(true);
+        },  CARET_BLINK_PAUSE_TIME_MS);
+    }
+
+    private setCaretBlink(value: boolean): void {
+        this.setState(Object.assign(this.state, {caretBlinking: value}));
     }
 }
