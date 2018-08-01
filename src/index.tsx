631437b081c246c7dc3491cb67fc6da2fccb3f34
Tomasz <tborcz@gmail.com>
2018-08-01 13:56:02 +0200

    Webpack css configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { h, render } from 'preact';
 import { App } from './app';
+import './index.scss';
 
 render(<App name='cool working' />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+const { join } = require('path');
+
 module.exports = {
-    entry: "./src/bootstrap.tsx",
+    entry: "./src/index.tsx",
     output: {
         filename: "./bundle.js",
     },
@@ -9,7 +11,33 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" }
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader" },
+            {
+                test: /\.scss?$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('autoprefixer')({
+                                    browsers: ['last 3 version']
+                                })
+                            ]
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: [
+                                join(__dirname, 'src')
+                            ]
+                        }
+                    }
+                ]
+            }
         ],
     },
 };
